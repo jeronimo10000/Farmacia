02cfad716fb12731bf9c387a342e4509ae69cbf3
jeronimo10000 <jeronimo000000@gmail.com>
2016-05-31 23:33:15 -0300

    Implementação de atualização de Clientes

--- src/farmacia/jdbc/TesteClienteDao.java
+++ src/farmacia/jdbc/TesteClienteDao.java
@@ -8,64 +8,65 @@ import farmacia.model.Clientes;
 public class TesteClienteDao {
 	public static void main(String[] args) {
 		
-		Scanner gravar = new Scanner(System.in);
-		
-		System.out.println("Digite o cpf: ");
-		String cpf = gravar.nextLine();
-		System.out.println("Digite o nome: ");
-		String nome = gravar.nextLine();
-		System.out.println("Digite o endereço: ");
-		String endereco = gravar.nextLine();
-		System.out.println("Digite o telefone: ");
-		String telefone = gravar.nextLine();
-		
-		// pronto para gravar
-		Clientes cliente = new Clientes();
-		cliente.setId(0);
-		cliente.setCpf(cpf);
-		cliente.setNome(nome);
-		cliente.setEndereco(endereco);
-		cliente.setTelefone(telefone);
-		
-		//grave nessa conexão
-		ClientesDao dao = new ClientesDao();
-		dao.adiciona(cliente);
-		
-		System.out.println("Gravado!");
-		
+//		Scanner gravar = new Scanner(System.in);
+//		
+//		System.out.println("Cadastro de Clientes!");
+//		System.out.println("Digite o cpf: ");
+//		String cpf = gravar.nextLine();
+//		System.out.println("Digite o nome: ");
+//		String nome = gravar.nextLine();
+//		System.out.println("Digite o endereço: ");
+//		String endereco = gravar.nextLine();
+//		System.out.println("Digite o telefone: ");
+//		String telefone = gravar.nextLine();
+//		
 //		// pronto para gravar
 //		Clientes cliente = new Clientes();
 //		cliente.setId(0);
-//		cliente.setCpf("045.256.256-89");
-//		cliente.setNome("jeronimo");
-//		cliente.setEndereco("Rua: Pedro da Costa Agra Nº 16");
-//		cliente.setTelefone("083 3365-9874");
+//		cliente.setCpf(cpf);
+//		cliente.setNome(nome);
+//		cliente.setEndereco(endereco);
+//		cliente.setTelefone(telefone);
 //		
 //		//grave nessa conexão
 //		ClientesDao dao = new ClientesDao();
 //		dao.adiciona(cliente);
 //		
 //		System.out.println("Gravado!");
+		
+		Scanner atualizar = new Scanner(System.in);
+		
+		System.out.println("Atualizar Cliente!");
+		System.out.println("Digite o cpf: ");
+		String cpf = atualizar.nextLine();
+		System.out.println("Digite o nome: ");
+		String nome = atualizar.nextLine();
+		System.out.println("Digite o endereço: ");
+		String endereco = atualizar.nextLine();
+		System.out.println("Digite o telefone: ");
+		String telefone = atualizar.nextLine();
+		System.out.println("Digite o código do Cliente");
+		int id = atualizar.nextInt();
+		
+		// pronto para atualizar
+		Clientes cliente = new Clientes();
+		cliente.setId(0);
+		cliente.setCpf(cpf);
+		cliente.setNome(nome);
+		cliente.setEndereco(endereco);
+		cliente.setTelefone(telefone);
+		cliente.setId(id);
 
-//		// pronto para gravar
-//		Clientes cliente = new Clientes();
-//		cliente.setId(1);
-//		cliente.setCpf("076.254.689-62");
-//		cliente.setNome("Cunha");
-//		cliente.setEndereco("Maria Honorato nº 36");
-//		cliente.setTelefone("083 3345-4793");
-//		cliente.setId(1);
-//
-//		// grave nessa conexão!!!
-//		ClientesDao dao = new ClientesDao();
-//
-//		// método elegante 
-//		dao.atualizar(cliente);
-//
-//		System.out.println("Atualizado!");
+		// grave nessa conexão!!!
+		ClientesDao dao = new ClientesDao();
+
+		// método elegante 
+		dao.atualizar(cliente);
+
+		System.out.println("Atualizado!");
 		
 		
-//		//pronto para atualizar
+//		//pronto para remover
 //		Clientes cliente = new Clientes();
 //		cliente.setId(2);
 //		
