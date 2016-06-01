e33dbb9cdf1a5200af37a1d97386b0d2e8355ca9
jeronimo10000 <jeronimo000000@gmail.com>
2016-06-01 00:16:58 -0300

    Pequenas mudanças na classe clientes

--- src/farmacia/model/Clientes.java
+++ src/farmacia/model/Clientes.java
@@ -2,12 +2,27 @@ package farmacia.model;
 
 public class Clientes extends Pessoa {
 
+	private Pessoa cliente;
+
+	public Clientes() {
+
+	}
+
 	public Clientes(int id, String cpf, String nome, String endereco, String telefone) {
 		super(id, cpf, nome, endereco, telefone);
 	}
 
-	public Clientes() {
-		// TODO Auto-generated constructor stub
+	public Clientes(Pessoa cliente) {
+		super();
+		this.cliente = cliente;
+	}
+
+	public Pessoa getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Pessoa cliente) {
+		this.cliente = cliente;
 	}
 
 }
