039ed7eedc6e0a08d66792a847a3aa890a7da9f0
jeronimo10000 <jeronimo000000@gmail.com>
2016-05-31 22:34:15 -0300

    Inserindo scanner em testeCliente

--- src/farmacia/jdbc/TesteClienteDao.java
+++ src/farmacia/jdbc/TesteClienteDao.java
@@ -1,24 +1,51 @@
 package farmacia.jdbc;
 
+import java.util.Scanner;
+
 import farmacia.jdbc.dao.ClientesDao;
 import farmacia.model.Clientes;
 
 public class TesteClienteDao {
 	public static void main(String[] args) {
 		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Digite o cpf: ");
+		String cpf = ler.next();
+		System.out.println("Digite o nome: ");
+		String nome = ler.next();
+		System.out.println("Digite o endereço: ");
+		String endereco = ler.next();
+		System.out.println("Digite o telefone: ");
+		String telefone = ler.next();
+		
 		// pronto para gravar
 		Clientes cliente = new Clientes();
 		cliente.setId(0);
-		cliente.setCpf("045.256.256-89");
-		cliente.setNome("jeronimo");
-		cliente.setEndereco("Rua: Pedro da Costa Agra Nº 16");
-		cliente.setTelefone("083 3365-9874");
+		cliente.setCpf(cpf);
+		cliente.setNome(nome);
+		cliente.setEndereco(endereco);
+		cliente.setTelefone(telefone);
 		
 		//grave nessa conexão
 		ClientesDao dao = new ClientesDao();
 		dao.adiciona(cliente);
 		
 		System.out.println("Gravado!");
+		
+//		// pronto para gravar
+//		Clientes cliente = new Clientes();
+//		cliente.setId(0);
+//		cliente.setCpf("045.256.256-89");
+//		cliente.setNome("jeronimo");
+//		cliente.setEndereco("Rua: Pedro da Costa Agra Nº 16");
+//		cliente.setTelefone("083 3365-9874");
+//		
+//		//grave nessa conexão
+//		ClientesDao dao = new ClientesDao();
+//		dao.adiciona(cliente);
+//		
+//		System.out.println("Gravado!");
 
 //		// pronto para gravar
 //		Clientes cliente = new Clientes();
